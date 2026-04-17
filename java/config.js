f174753dc728d6c0470194933fba69ca6db203e0
Murilo 26 <00001114982313sp@al.educacao.sp.gov.br>
2026-04-17 15:18:08 -0300

    Change planilhalivros URL to use CORS proxy
    Updated the URL for planilhalivros to use a CORS proxy.

--- java/config.js
+++ java/config.js
@@ -1,1 +1,4 @@
-const planilhalivros = "https://script.google.com/macros/s/AKfycbyQ8dhhtqiHKLh52HPR9vQZtnz6ymJBLDX8EBEmwDJukkYYy0ZaT48hB64AhNVubfU2/exec";
+
+// Antes você tinha: https://script.google.com/macros/s/SEU_ID/exec
+// Agora use:
+const planilhalivros = "https://corsproxy.io/?url=" + encodeURIComponent("https://script.google.com/macros/s/AKfycbyQ8dhhtqiHKLh52HPR9vQZtnz6ymJBLDX8EBEmwDJukkYYy0ZaT48hB64AhNVubfU2/exec");
