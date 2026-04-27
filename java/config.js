0a52cf6877dca66be9d453f2763e5015d025e8a7
Murilo 26 <00001114982313sp@al.educacao.sp.gov.br>
2026-04-27 17:09:28 -0300

    Delete java/config.js

--- java/config.js
+++
@@ -1,4 +0,0 @@
-
-// Antes você tinha: https://script.google.com/macros/s/SEU_ID/exec
-// Agora use:
-//const planilhalivros = "https://corsproxy.io/?url=" + encodeURIComponent("https://script.google.com/macros/s/AKfycbyQ8dhhtqiHKLh52HPR9vQZtnz6ymJBLDX8EBEmwDJukkYYy0ZaT48hB64AhNVubfU2/exec");
